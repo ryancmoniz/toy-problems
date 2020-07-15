8af0e132044bc44788b7631875d7c9eab16a19e1
ryancmoniz <ryancmoniz@gmail.com>
2020-07-15 12:49:40 -0400

    Add depthFirstSearch

---
+++ Easy/depthFirstSearch/depthFirstSearch.go
@@ -0,0 +1,14 @@
+package dfs
+
+type Node struct {
+	Name     string
+	Children []*Node
+}
+
+func (n *Node) DepthFirstSearch(array []string) []string {
+	array = append(array, n.Name)
+	for _, child := range n.Children {
+		array = child.DepthFirstSearch(array)
+	}
+	return array
+}
