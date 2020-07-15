41642d6457400cafac9db62db95c12fc048956c5
Ryan Moniz <ryancmoniz@gmail.com>
2020-07-15 12:41:40 -0400

    Update bubbleSort.go

--- Easy/bubbleSort/Go/bubbleSort.go
+++ Easy/bubbleSort/Go/bubbleSort.go
@@ -1,12 +1,12 @@
 package bubblesort
 
 func bubbleSort(array []int) []int {
-for i := len(array); i > 0; i-- {
-	for j := 0; j < i-1; j++ {
-		if array[j] > array[j+1] {
-			array[j], array[j+1] = array[j+1], array[j]
-		}
-	}
-}
-return array
+    for i := len(array); i > 0; i-- {
+        for j := 0; j < i-1; j++ {
+            if array[j] > array[j+1] {
+	            array[j], array[j+1] = array[j+1], array[j]
+            }
+        }
+    }
+    return array
 }
