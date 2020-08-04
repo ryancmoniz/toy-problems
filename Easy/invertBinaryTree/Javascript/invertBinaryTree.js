91f2ceac47f0c8d0839a2ca8807610ca1dc1b3d1
ryancmoniz <ryancmoniz@gmail.com>
2020-08-03 20:59:58 -0400

    add invertBinaryTree, Javascript

---
+++ Easy/invertBinaryTree/Javascript/invertBinaryTree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+function invertTree(root) {
+  if (root) {
+    if (root.left) {
+      invertTree(root.left);
+    }
+
+    if (root.right) {
+      invertTree(root.right);
+    }
+    [root.left, root.right] = [root.right, root.left];
+  }
+  return root;
+}
